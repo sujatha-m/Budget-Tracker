956e8e83aa32da85b7b500109436c290fb927872
sujatha <jhanavi.bhushan@gmail.com>
2020-06-18 18:24:45 -0400

    Changed the images size for heroku app download

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,8 +5,8 @@ const FILES_TO_CACHE = [
     '/index.js',
     '/manifest.webmanifest',
     '/styles.css',
-    '/icons/icon-192x169.png',
-    '/icons/icon-512x452.png'
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
   ];
 const CACHE_NAME = 'static-cache-v2'
 const DATA_CACHE_NAME = 'data-cache-v1'
